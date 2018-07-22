29192ec1c70dae480a1c2373740aa4dc93f3c737
Travis CI User <travis@example.org>
2018-07-22 18:33:31 +0000

    Automatic Travis documentation build
    Add methods for `Punctuated`
    
    Also add unit tests for this type.

---
+++ master/implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tl_lang_syn"] = [{text:"impl&lt;T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"tl_lang_syn/punctuated/struct.Punctuated.html\" title=\"struct tl_lang_syn::punctuated::Punctuated\">Punctuated</a>&lt;T, P&gt;",synthetic:false,types:["tl_lang_syn::punctuated::Punctuated"]},{text:"impl&lt;'a, T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"tl_lang_syn/punctuated/struct.Punctuated.html\" title=\"struct tl_lang_syn::punctuated::Punctuated\">Punctuated</a>&lt;T, P&gt;",synthetic:false,types:["tl_lang_syn::punctuated::Punctuated"]},{text:"impl&lt;'a, T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a mut <a class=\"struct\" href=\"tl_lang_syn/punctuated/struct.Punctuated.html\" title=\"struct tl_lang_syn::punctuated::Punctuated\">Punctuated</a>&lt;T, P&gt;",synthetic:false,types:["tl_lang_syn::punctuated::Punctuated"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/tl_lang_syn/error/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ParseError",""]],"type":[["ParseResult",""]]});
