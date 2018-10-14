f9f7b4d288fedc1cbae91798df4d635e366ca84b
Travis CI User <travis@example.org>
2018-10-14 23:01:50 +0000

    Automatic Travis documentation build
    Add more impls
    
    Also lock `nom` on 4.0.0 because `nom` 4.1.0 has impls for 128-bit
    integers which require Rust 1.26.

---
+++ master/implementors/core/ops/index/trait.IndexMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tl_lang_syn"] = [{text:"impl&lt;T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.IndexMut.html\" title=\"trait core::ops::index::IndexMut\">IndexMut</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"tl_lang_syn/punctuated/struct.Punctuated.html\" title=\"struct tl_lang_syn::punctuated::Punctuated\">Punctuated</a>&lt;T, P&gt;",synthetic:false,types:["tl_lang_syn::punctuated::Punctuated"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
