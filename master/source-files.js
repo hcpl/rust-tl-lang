c615919445cf1901cb00d9b95710e314ceeb99d9
Travis CI User <travis@example.org>
2019-05-11 23:21:24 +0000

    Automatic Travis documentation build
    Bump minimum Rust version to 1.24
    
    This matches MSRV of `nom` 4.2.3.

---
+++ master/source-files.js
@@ -0,0 +1,11 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["either"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["macro_attr"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","lib.rs","naive.rs"]};
+sourcesIndex["nom"] = {"name":"","files":["bits.rs","branch.rs","bytes.rs","character.rs","internal.rs","lib.rs","macros.rs","methods.rs","multi.rs","nom.rs","sequence.rs","str.rs","traits.rs","types.rs","util.rs","verbose_errors.rs","whitespace.rs"]};
+sourcesIndex["proc_macro2"] = {"name":"","files":["fallback.rs","lib.rs","strnom.rs","wrapper.rs"]};
+sourcesIndex["quote"] = {"name":"","files":["ext.rs","lib.rs","runtime.rs","to_tokens.rs"]};
+sourcesIndex["tl_lang_rust_interop"] = {"name":"","files":["constructor_def.rs","constructor_variant.rs","field.rs","function_def.rs","ident.rs","lib.rs","path.rs","schema.rs","token_generator.rs","ty.rs","type_def.rs","utils.rs"]};
+sourcesIndex["tl_lang_syn"] = {"name":"","files":["bit.rs","comment.rs","cursor.rs","error.rs","file.rs","id.rs","ident.rs","item.rs","lib.rs","parsers.rs","path.rs","punctuated.rs","span.rs","spanned.rs","synom.rs","token.rs","ty.rs","utils.rs"]};
+sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
+createSourceSidebar();
